5bf4e6e4973999da11813086bed4087d2ae36d49
Jonas Garcia rosa <jgarcia.rosa@gmail.com>
2024-06-18 14:45:16 -0300

    adding packages, creating infra project and PersonContext

---
+++ Infrastructure/Data/PersonContext.cs
@@ -0,0 +1,11 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class PersonContext : DbContext
+{
+    public PersonContext(DbContextOptions options) : base(options) { }
+
+    public DbSet<Person> Persons { get; set; }
+}
