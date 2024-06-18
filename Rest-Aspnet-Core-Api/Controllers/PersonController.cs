e70b03fb70211e3b4c057d052a236a47161e5fab
Jonas Garcia rosa <jgarcia.rosa@gmail.com>
2024-06-18 16:10:10 -0300

    creating CreatePerson, GetPerson and GetPersonById endpoints

--- Rest-Aspnet-Core-Api/Controllers/PersonController.cs
+++ Rest-Aspnet-Core-Api/Controllers/PersonController.cs
@@ -1,6 +1,63 @@
-﻿namespace Rest_Aspnet_Core_Api.Controllers;
+﻿using Core.Models;
+using Infrastructure.Data;
+using Infrastructure.Data.Dtos;
+using Microsoft.AspNetCore.Mvc;
 
-public class PersonController
+namespace Rest_Aspnet_Core_Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PersonController : ControllerBase
 {
+    private PersonContext _context;
 
+    public PersonController(PersonContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    public IActionResult CreatePerson([FromBody] CreatePersonDto personDto)
+    {
+        Person person = new Person
+        {
+            FirstName = personDto.FirstName,
+            LastName = personDto.LastName,
+            Gender = personDto.Gender
+        };
+        _context.Persons.Add(person);
+        _context.SaveChanges();
+        return CreatedAtAction(nameof(GetPersonById), new { id = person.Id }, person);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetPersonById(int id)
+    {
+        var person = _context.Persons.FirstOrDefault(person => person.Id == id);
+        if(person == null) return NotFound();
+        ReadPersonDto personDto = new ReadPersonDto
+        {
+            FirstName = person.FirstName,
+            LastName = person.LastName,
+            Gender = person.Gender
+        };
+        return Ok(personDto);
+    }
+
+    [HttpGet]
+    public IEnumerable<ReadPersonDto> GetPersons()
+    {
+        List<ReadPersonDto> personDtos = new List<ReadPersonDto>();
+        foreach(Person person in _context.Persons)
+        {
+            ReadPersonDto personDto = new ReadPersonDto
+            {
+                FirstName = person.FirstName,
+                LastName = person.LastName,
+                Gender = person.Gender
+            };
+            personDtos.Add(personDto);
+        }
+        return personDtos;
+    }
 }
