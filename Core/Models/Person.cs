536c2aa3fefc03f65eb2bfedca78a51e4db9c397
Jonas Garcia rosa <jgarcia.rosa@gmail.com>
2024-06-18 10:50:25 -0300

    creating Core project and creating Person Model

---
+++ Core/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models;
+
+public class Person
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+    [Required, MaxLength(20)]
+    public string? FirstName { get; set; }
+    [Required, MaxLength(50)]
+    public string? LastName { get; set; }
+    [Required, MaxLength(9)]
+    public string? Gender { get; set; }
+}
