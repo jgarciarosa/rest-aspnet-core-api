1ad838ff0ecd0fee6e5a9a79ed592f0159b31312
Jonas Garcia rosa <jgarcia.rosa@gmail.com>
2024-06-18 15:44:36 -0300

    creating person dtos and PersonController

---
+++ Infrastructure/Data/Dtos/UpdatePersonDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Data.Dtos;
+
+public class UpdatePersonDto
+{
+    [Required, StringLength(20)]
+    public string? FirstName { get; set; }
+    [Required, StringLength(50)]
+    public string? LastName { get; set; }
+    [Required, StringLength(9)]
+    public string? Gender { get; set; }
+}
---
+++ Rest-Aspnet-Core-Api/Controllers/PersonController.cs
@@ -0,0 +1,6 @@
+﻿namespace Rest_Aspnet_Core_Api.Controllers;
+
+public class PersonController
+{
+
+}
